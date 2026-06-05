e8f7f170798acff513ae5bfe040f64875c9e95f5
Hamza Nadeem <117504957+hamzanadeem99@users.noreply.github.com>
2026-06-05 16:07:08 +0500

    Create Bank.java

---
+++ Task2-Inheritance/Bank.java
@@ -0,0 +1,15 @@
+class Bank {
+    private String name;
+    Bank(String name) {
+        this.name = name;
+    }
+
+    // Getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
